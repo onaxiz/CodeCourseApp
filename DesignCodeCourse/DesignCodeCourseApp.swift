67bc8e6ef512b7ab8752dfee0d4c4683970937f6
emaksimova <maksimovaswift@gmail.com>
2025-04-05 22:37:50 +0300

    Initial Commit

---
+++ DesignCodeCourse/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  DesignCodeCourse
+//
+//  Created by Евгения Максимова on 05.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
