abc9a5624c8425f687b449a9c1ed86070a84ace9
emaksimova <maksimovaswift@gmail.com>
2025-04-06 12:52:26 +0300

    design, icons, colors

--- DesignCodeCourse/ContentView.swift
+++ DesignCodeCourse/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading, spacing: 4) {
+            Image("Illustration 1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Text("SwiftUI for iOS 14")
+                .font(.footnote)
+            Text("20 sections")
+                .font(.footnote)
         }
-        .padding()
+        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        .frame(height: 200.0)
+        .background(Color.blue)
+        .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+        
     }
 }
 
